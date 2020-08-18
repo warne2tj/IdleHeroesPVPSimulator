e33ac89c6549ffa1b278fa00a3620273ce9d1bac
AngryRabbit1 <t101000@yahoo.com>
2020-08-17 23:04:54 -0700

    Update seededHeroes.js

--- GeneticAlgorithmMulti/seededHeroes.js
+++ GeneticAlgorithmMulti/seededHeroes.js
@@ -1,217 +1,217 @@
 // Dictionary of heroes containing list of allowed enables, artifacts, equipments, and enables
 var seededHeroes = {
   "Aida": {
-    allowedArtifacts: ["Golden Crown"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Wildfire Torch"],
     allowedStones: ["HP, HP"],
     allowedEquipments: ["Split HP"],
     allowedEnables: ['"Vitality", "Shelter", "Purify", "Vitality", "UnbendingWill"','"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
   },
   "Amen-Ra": {
-    allowedArtifacts: ["Golden Crown"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Wildfire Torch"],
     allowedStones: ["HP, HP"],
     allowedEquipments: ["Split HP"],
     allowedEnables: ['"Vitality", "Shelter", "Purify", "Vitality", "UnbendingWill"','"Vitality", "Vitality2", "Purify", "Vitality", "UnbendingWill"']
   },
   "Amuvor": {
-    allowedArtifacts: ["Staff Punisher of Immortal"],
+    allowedArtifacts: ["Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Wildfire Torch"],
     allowedStones: ["Speed, Crit","Crit, Crit, Attack"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Asmodel": {
-    allowedArtifacts: ["The Kiss of Ghost","Staff Punisher of Immortal"],
+    allowedArtifacts: ["The Kiss of Ghost","Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Wildfire Torch"],
     allowedStones: ["Crit, Crit, Attack","Attack, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Aspen": {
-    allowedArtifacts: ["Golden Crown","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["Crit, Crit, Attack"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Belrain": {
-    allowedArtifacts: ["Augustus Magic Ball","Antlers Cane","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Antlers Cane","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["Attack, Attack","HP, Heal"],
     allowedEquipments: ["Class Gear","Split Attack"],
     allowedEnables: ['"Mightiness", "LethalFightback", "SharedFate", "Mightiness", "BalancedStrike"','"Growth", "Vitality2", "Purify", "Growth", "UnbendingWill"']
   },
   "Carrie": {
-    allowedArtifacts: ["Augustus Magic Ball","Demon Bell","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, HP","Speed, Attack"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"','"Growth", "LethalFightback", "Purify", "Growth", "BalancedStrike"']
   },
   "Cthugha": {
-    allowedArtifacts: ["Augustus Magic Ball","Golden Crown"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Wildfire Torch"],
     allowedStones: ["HP, Attack","HP, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Dark Arthindol": {
-    allowedArtifacts: ["Staff Punisher of Immortal","Demon Bell","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Demon Bell","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, Crit","Crit, Crit, Attack"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Delacium": {
-    allowedArtifacts: ["Staff Punisher of Immortal","Golden Crown","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["Speed, HP","HP, Attack","HP, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"','"Vitality", "Vitality2", "Resilience", "Vitality", "UnbendingWill"']
   },
   "Drake": {
-    allowedArtifacts: ["Augustus Magic Ball","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, HP","Speed, Attack"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Elyvia": {
-    allowedArtifacts: ["Golden Crown"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Wildfire Torch"],
     allowedStones: ["Speed, HP","HP, HP"],
     allowedEquipments: ["Split HP"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
   },
   "Emily": {
-    allowedArtifacts: ["Demon Bell"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell","Wildfire Torch"],
     allowedStones: ["Speed, HP"],
     allowedEquipments: ["Split HP"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
   },
   "Faith Blade": {
-    allowedArtifacts: ["Augustus Magic Ball","Staff Punisher of Immortal","Demon Bell"],
+    allowedArtifacts: ["Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Demon Bell","Wildfire Torch"],
     allowedStones: ["Speed, Crit","HP, Attack","HP, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Garuda": {
-    allowedArtifacts: ["Golden Crown","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["Attack, Attack, Holy","HP, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Vitality", "Shelter", "Purify", "Vitality", "UnbendingWill"','"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Gustin": {
-    allowedArtifacts: ["Golden Crown"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Wildfire Torch"],
     allowedStones: ["Speed, HP","HP, HP"],
     allowedEquipments: ["Split HP"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"','"Vitality", "Vitality2", "Resilience", "Vitality", "UnbendingWill"']
   },
   "Horus": {
-    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["HP, Attack","HP, Attack, Holy","HP, Block"],
     allowedEquipments: ["Class Gear","Split HP"],
     allowedEnables: ['"Vitality", "Shelter", "Purify", "Vitality", "UnbendingWill"']
   },
   "Ithaqua": {
-    allowedArtifacts: ["The Kiss of Ghost","Augustus Magic Ball","Staff Punisher of Immortal"],
+    allowedArtifacts: ["The Kiss of Ghost","Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Wildfire Torch"],
     allowedStones: ["Speed, Attack","Speed, Crit"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"','"Mightiness", "LethalFightback", "Purify", "Mightiness", "UnbendingWill"']
   },
   "Kroos": {
-    allowedArtifacts: ["Demon Bell","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, HP"],
     allowedEquipments: ["Split HP"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
   },
   "Michelle": {
-    allowedArtifacts: ["Demon Bell","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, Attack","Speed, Crit"],
     allowedEquipments: ["Class Gear","Split HP"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
   },
   "Mihm": {
-    allowedArtifacts: ["The Kiss of Ghost","Antlers Cane"],
+    allowedArtifacts: ["The Kiss of Ghost","Augustus Magic Ball","Golden Crown","Antlers Cane","Wildfire Torch"],
     allowedStones: ["Attack, Attack, Holy","HP, Attack, Holy"],
     allowedEquipments: ["Class Gear","Split Attack"],
     allowedEnables: ['"Mightiness", "Shelter", "Resilience", "Mightiness", "UnbendingWill"','"Vitality", "Shelter", "Resilience", "Vitality", "UnbendingWill"']
   },
   "Nakia": {
-    allowedArtifacts: ["Augustus Magic Ball"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Wildfire Torch"],
     allowedStones: ["Speed, Attack","Speed, Crit"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Oberon": {
-    allowedArtifacts: ["Demon Bell","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, HP"],
     allowedEquipments: ["Split HP"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
   },
   "Penny": {
-    allowedArtifacts: ["The Kiss of Ghost","Staff Punisher of Immortal"],
+    allowedArtifacts: ["The Kiss of Ghost","Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Wildfire Torch"],
     allowedStones: ["Speed, HP","Speed, Crit","Crit, Crit, Attack"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"','"Vitality", "Vitality2", "Resilience", "Vitality", "UnbendingWill"']
   },
   "Rogan": {
-    allowedArtifacts: ["Golden Crown","Demon Bell"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell","Wildfire Torch"],
     allowedStones: ["Speed, HP","HP, HP"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Vitality", "Shelter", "Purify", "Vitality", "UnbendingWill"','"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Russell": {
-    allowedArtifacts: ["Golden Crown","Demon Bell"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell","Wildfire Torch"],
     allowedStones: ["Speed, HP","HP, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Vitality", "Shelter", "Purify", "Vitality", "UnbendingWill"','"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Sherlock": {
-    allowedArtifacts: ["Lucky Candy Bar","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Lucky Candy Bar","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, HP","Speed, Attack"],
     allowedEquipments: ["Class Gear","No Armor"],
     allowedEnables: ['"Growth", "Shelter", "Resilience", "Growth", "UnbendingWill"','"Vitality", "Shelter", "Resilience", "Vitality", "UnbendingWill"']
   },
   "Tara": {
-    allowedArtifacts: ["Golden Crown","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["Attack, Attack, Holy","HP, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"','"Vitality", "Shelter", "Purify", "Vitality", "UnbendingWill"']
   },
   "UniMax-3000": {
-    allowedArtifacts: ["Augustus Magic Ball","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, HP","HP, Attack","HP, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"','"Vitality", "Vitality2", "Resilience", "Vitality", "UnbendingWill"']
   },
   "Ormus": {
-    allowedArtifacts: ["Augustus Magic Ball","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["Attack, Attack","HP, Heal"],
     allowedEquipments: ["Class Gear","Split Attack"],
     allowedEnables: ['"Mightiness", "LethalFightback", "SharedFate", "Mightiness", "BalancedStrike"','"Growth", "Vitality2", "Purify", "Growth", "UnbendingWill"']
   },
   "Valkryie": {
-    allowedArtifacts: ["Golden Crown"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Wildfire Torch"],
     allowedStones: ["HP, HP"],
     allowedEquipments: ["Split HP"],
     allowedEnables: ['"Vitality", "Shelter", "Purify", "Vitality", "UnbendingWill"']
   },
   "Gerke": {
-    allowedArtifacts: ["Golden Crown","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["Attack, Attack"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
   },
   "Sleepless": {
-    allowedArtifacts: ["Golden Crown","Magic Stone Sword"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Magic Stone Sword","Wildfire Torch"],
     allowedStones: ["Attack, Attack, Holy"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
   },
   "Das Moge": {
-    allowedArtifacts: ["Staff Punisher of Immortal"],
+    allowedArtifacts: ["Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Wildfire Torch"],
     allowedStones: ["Crit, Crit, Attack"],
     allowedEquipments: ["Class Gear"],
     allowedEnables: ['"Growth", "LethalFightback", "Purify", "Growth", "UnbendingWill"']
   },
   "Ignis": {
-    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell"],
+    allowedArtifacts: ["Augustus Magic Ball","Golden Crown","Demon Bell","Wildfire Torch"],
     allowedStones: ["Speed, HP"],
     allowedEquipments: ["Class Gear","Split HP"],
     allowedEnables: ['"Growth", "Vitality2", "Purify", "Growth", "UnbendingWill"']
   },
   "Heart Watcher": {
-    allowedArtifacts: ["Staff Punisher of Immortal","Golden Crown","Ruyi Scepter"],
+    allowedArtifacts: ["Augustus Magic Ball","Staff Punisher of Immortal","Golden Crown","Ruyi Scepter","Wildfire Torch"],
     allowedStones: ["Speed, HP","Speed, Crit"],
     allowedEquipments: ["Class Gear","Split HP"],
     allowedEnables: ['"Growth", "Shelter", "Purify", "Growth", "UnbendingWill"']
